708a6c8cbacd8586275077ee76c7fc58bcaca82d
EzequielGomez74 <ezequiel.canob@gmail.com>
2022-10-21 22:17:12 -0300

    index api

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,4 +1,6 @@
 const { Router } = require('express');
+const countries = require('./countries')
+const activities = require('./activities')
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -7,6 +9,7 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+router.use('/', countries);
+router.use('/', activities);
 
 module.exports = router;
