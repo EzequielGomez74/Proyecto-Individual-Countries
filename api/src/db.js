d4fb05aa3611a1cf7525952570639592c64c8056
EzequielGomez74 <ezequiel.canob@gmail.com>
2023-02-01 03:07:19 -0300

    deploy1

--- api/src/db.js
+++ api/src/db.js
@@ -5,6 +5,8 @@ const path = require('path');
 const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY} = process.env;
 
+  //NO DESCOMENTAR
+
 // var sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, {
 //   logging: false, // set to console.log to see the raw SQL queries
 //   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
